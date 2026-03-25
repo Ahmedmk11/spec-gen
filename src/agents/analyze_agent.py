8fd921024a50b870afbe9b405181d92af67e043c
Ahmed Mahmoud <ahmedmahmoudkzm@gmail.com>
2026-03-25 23:27:42 +0200

    feat: add agent starter classes and implement AgentState

--- src/agents/analyze_agent.py
+++ src/agents/analyze_agent.py
@@ -1,19 +1,38 @@
 import os
+import textwrap
 
 from langchain.chat_models import init_chat_model
+from langchain_core.callbacks import UsageMetadataCallbackHandler
 
-# def get_env_model():
-#     model = os.getenv("ANTHROPIC_MODEL")
-#     if not model:
-#         raise ValueError("Model is not set")
-#     return model
+def get_env_model():
+    model = os.getenv("ANTHROPIC_MODEL")
+    if not model:
+        raise ValueError("Model is not set")
+    return model
 
-# class LLMClient:
-#     def __init__(self, temperature: float = 0, tools: list | None = None) -> None:
-#         self.llm = init_chat_model(
-#             model=get_env_model(),
-#             temperature=temperature,
-#             model_kwargs={
-#                 "cache_control": {"type": "ephemeral"}
-#             },
-#         ).bind_tools(tools if tools else [])
+class LLMClient:
+    def __init__(self, tools: list | None = None) -> None:
+        self.llm = init_chat_model(
+            model=get_env_model(),
+            model_kwargs={
+                "cache_control": {"type": "ephemeral"}
+            },
+        ).bind_tools(tools if tools else [])
+
+class AnalyzeAgent:
+    def __init__(self, tools: list | None = None, mcp_tools: list | None = None) -> None:
+        self.usage_callback = UsageMetadataCallbackHandler()
+
+        self.local_tools = tools if tools else []
+        self.mcp_tools = mcp_tools if mcp_tools else []
+        self.tools = [*self.local_tools, *self.mcp_tools]
+
+        self.llm = LLMClient(tools=self.tools)
+        self.graph = self._build_graph()
+
+        self.system_prompt = textwrap.dedent("""
+        Placeholder
+        """).strip()
+
+    async def ainvoke(self, max_iterations: int = 32):
+        pass
---
+++ src/state.py
@@ -0,0 +1,15 @@
+from typing import TypedDict
+
+class AgentState(TypedDict):
+    code: str # code to generate tests for
+    tests: str # generated tests file for the current function
+
+    status: str # status of last test file
+    output: str # output of last test file
+
+    decision: str # decision of the analysis agent on the last test file
+    reason: str # reason of last decision
+
+    previous_attempts: list[str] # previous attempts of creating the current test file, empty if first attempt
+
+    retries: int
